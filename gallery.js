48a5b9f3df53f707e4a81eb8a8495cd1da4de8b8
viktus1233 <161202839+viktus1233@users.noreply.github.com>
2024-04-21 21:00:16 +0200

    Delete gallery.js

--- gallery.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    // Načítanie obrázkov z JSON súboru
-    $.getJSON("gallery.json", function(data){
-        $.each(data.images, function(index, image){
-            $("#gallery").append("<div class='image'><img src='" + image.url + "' alt='" + image.alt + "'></div>");
-        });
-    });
-});
